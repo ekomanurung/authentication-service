4a3c3257f7f3896c1d704d782f5b9aa51eee4c63
Eko Januardy <eko.j.manurung@gmail.com>
2018-01-20 18:48:26 +0700

    init setup

---
+++ configuration/src/main/java/SwaggerConfiguration.java
@@ -0,0 +1,46 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("travelmart")
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(Arrays.asList(
+                        new ParameterBuilder().name("channelId").parameterType("header").modelRef(new ModelRef("string"))
+                                .required(true).defaultValue("").description("client's channel id").build(),
+                        new ParameterBuilder().name("username").parameterType("header").modelRef(new ModelRef("string"))
+                                .required(false).description("client's username").build(),
+                        new ParameterBuilder().name("requestId").parameterType("header").modelRef(new ModelRef("string"))
+                                .required(true).defaultValue("").description("unique id per request").build()
+                ));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring REST API with Swagger")
+                .description("Sample Authentication Service")
+                .contact("surung-mengajar")
+                .build();
+    }
+}
---
+++ configuration/src/main/java/WebConfiguration.java
@@ -0,0 +1,6 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfiguration  extends WebMvcConfigurerAdapter{
+}
